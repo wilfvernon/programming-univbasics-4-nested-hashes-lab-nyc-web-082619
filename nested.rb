54600e9853f0f44bdc192fc69ccefcb60bdb32d0
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-21 19:56:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,13 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +54,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +80,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages][] = ["LISP", "C"]
     
 end
 
