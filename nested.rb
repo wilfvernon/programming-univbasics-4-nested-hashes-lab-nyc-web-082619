6a5cc0e98823d7d972b28b905f56248a95e046c0
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-21 20:13:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
         }
      }
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay].update(:known_for, alans_new_info)
+     programmer_hash.update(:alan_kay[:known_for], alans_new_info)
      
      
 end
