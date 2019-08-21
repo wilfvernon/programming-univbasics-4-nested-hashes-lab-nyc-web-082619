c900da1ddf158b225f4563ab2c841345ac2917ce
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-21 20:16:26 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,13 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +54,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,8 +80,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  programmer_hash
 end
 
 def changing_alan
@@ -99,8 +103,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
      
 end
@@ -121,5 +126,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     programmer_hash
 
 end
